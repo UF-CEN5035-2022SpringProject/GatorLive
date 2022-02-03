e8eb3305b444c45767fa996290b58ee44f654955
Yi-Ming Chang <69064626+yimingStar@users.noreply.github.com>
2022-02-02 23:44:26 -0500

    adding db connection function

---
+++ backend/src/github.com/UF-CEN5035-2022SpringProject/GatorStore/db/db.go
@@ -0,0 +1,25 @@
+package db
+
+import (
+	"context"
+	"log"
+
+	firebase "firebase.google.com/go"
+	"google.golang.org/api/option"
+)
+
+func ConnectionSetUp() {
+	// Use a service account
+	ctx := context.Background()
+	sa := option.WithCredentialsFile("path/to/serviceAccount.json")
+	app, err := firebase.NewApp(ctx, nil, sa)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	client, err := app.Firestore(ctx)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer client.Close()
+}
