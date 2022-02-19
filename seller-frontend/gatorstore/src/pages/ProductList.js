4403a78c89b5e67c65b946a901231ead6037bb9e
llerenabarruetos <40399062+llerenabarruetos@users.noreply.github.com>
2022-02-19 12:46:51 -0500

    Code Rearranging, test JSON streamObject, new overlay for selecting products for stream
    Combined all 4 overlays into 1 function, now only using 1 setState variable to see which overlay to display.
    Testing switching between "live" and "not-live" states with a test JSON file that represents the database.
    Made new overlay (work-in-progress) for choosing the items to sell in the new stream.

--- seller-frontend/gatorstore/src/pages/ProductList.js
+++ seller-frontend/gatorstore/src/pages/ProductList.js
@@ -10,19 +10,40 @@ import CircleIcon from '@mui/icons-material/Circle';
 import ContentCopyIcon from '@mui/icons-material/ContentCopy';
 import '../styles/sellerStorePage.css';
 
+import testStreamObject from '../test-data/streamObject.json';
+
 function ProductList() {
-  var storeObject = {id: "2", isLive: false}; // TEST ID
-  CheckStoreObject(); // initial check
+  var storeObject = {id: "2", isLive: false}; // TEST: it's a local replica of the test streamObject.json (represents Database)
 
   const [liveInfoBarState, SetLiveInfoBarState] = useState('not-live');
 
   // Checks the status of this store's object in the store API:
   function CheckStoreObject() { 
-    // storeObject = fetch and stuff from API
+    /* call API - TODO: When Yiming finishes this API. Store the streamObject in variable storeObject
+    const requestOptions = {
+      method: 'POST',
+      header: {'jwtToken': jwtToken},
+      body: JSON.stringify({ title: sTitle }) 
+    };
+    fetch('http://10.136.228.201:8080/store/'+ storeObject.id +'/livestream', requestOptions)
+        .then(response => response.json())
+        .then(response => {
+          if (response.status === 0) {
+            SetStreamObject({key: response.result.streamKey, url: response.result.streamUrl});
+          } else {
+            alert("ERROR: YouTube API did not respond with 'success' status code 0.");
+          }
+        })
+        .catch((error) => {
+            console.error(error);
+        });*/
+
+    storeObject.isLive = testStreamObject.isLive;
     
     if (storeObject.isLive === true) {
-      alert("is live!");
       SetLiveInfoBarState('live');
+    } else {
+      SetLiveInfoBarState('not-live');
     }
   }
 
@@ -30,12 +51,12 @@ function ProductList() {
   useEffect(() => {
     const interval = setInterval(() => {
       CheckStoreObject();
-    }, 60000);
+    }, 15000);
   
     return () => clearInterval(interval); // This represents the unmount function, in which you need to clear your interval to prevent memory leaks.
   }, [])
 
-  // Overlays' hooks:
+  /* Overlays' hooks:
   const [successOverlayDisplayed, ChangeSuccessOverlayDisplayed] = useState("none");
   const [titleOverlayDisplayed, ChangeTitleOverlayDisplayed] = useState("none");
 
@@ -45,8 +66,98 @@ function ProductList() {
   useEffect(() => {
     document.body.style.overflowY = (successOverlayDisplayed !== "none") ? "hidden" : "scroll";
   }, [successOverlayDisplayed]);
+*/
 
-  // Overlay displaying input for stream title:
+  // High IQ test:
+  const [currentOverlay, ChangeCurrentOverlay] = useState("none");
+  useEffect(() => {
+    document.body.style.overflowY = (currentOverlay !== "none") ? "hidden" : "scroll";    
+  }, [currentOverlay]);
+
+  function Overlay() {
+    const [streamTitle, SetStreamTitle] = useState("");
+
+    return(
+      <div>
+        {currentOverlay === 'setStreamTitle' && (
+          <div style={{display: 'none'}} />
+        )}
+
+        {currentOverlay === 'setStreamTitle' && (
+          <div class="go-live-overlay">
+            <div class="transparentBG"/>
+            <div class="stream-link-container" style={{top: '35vh'}}>
+              <p>Enter a title for your stream</p>
+              <TextField id="titleField" variant="outlined" color="primary" placeholder="Title" size="small" onChange={(e) => SetStreamTitle(e.target.value)} style={{width: "100%", marginBottom: 15}}/>
+              <div style={{textAlign: "center"}}>
+                <Button variant="contained" color="warning" onClick={() => {
+                    //ChangeTitleOverlayDisplayed("none");
+                    ChangeCurrentOverlay("none");
+                }} size="large" style={{marginRight: "10%"}}>Cancel</Button>
+                
+                <Button variant="contained" color="primary" onClick={() => {
+                  if (streamTitle !== "") { // if a title was typed
+                      ChangeCurrentOverlay("selectStreamProducts");
+                  } else { // give warning that title is required
+                    document.getElementById("titleField").style.border = "red solid 2px";
+                    document.getElementById("titleField").placeholder = "Title is Required";
+                  }
+                }} size="large">Continue</Button>
+              </div>
+            </div>
+          </div>
+        )}
+
+        {currentOverlay === 'selectStreamProducts' && (
+          <div class="go-live-overlay" >
+            <div class="transparentBG"/>
+            <div class="stream-link-container">
+              <h1>What Products Will You Showcase?</h1>
+              
+              <div>
+                nuts
+              </div>
+    
+              <div style={{textAlign: "center"}}>
+                <Button variant="contained" color="primary" onClick={() => { 
+                  // call YT API with this title: 
+                  GoLive(streamTitle);
+                  
+                  ChangeCurrentOverlay("showStreamCreated");
+                }} size="large">Continue</Button>
+              </div>
+            </div>
+          </div>
+        )}
+
+        {currentOverlay === 'showStreamCreated' && (
+          <div class="go-live-overlay" >
+            <div class="transparentBG"/>
+            <div class="stream-link-container">
+              <h1>Stream Created!</h1>
+              <p>URL</p>
+              <div class="stream-url-box">
+                <p>{newStream.url}</p>
+                <Button variant="contained" color="secondary" onClick={() => {navigator.clipboard.writeText(newStream.url)}}><ContentCopyIcon/></Button>
+              </div>
+    
+              <p>Key</p>
+              <div class="stream-url-box">
+                <p>{newStream.key}</p>
+                <Button variant="contained" color="secondary" onClick={() => {navigator.clipboard.writeText(newStream.key)}}><ContentCopyIcon/></Button>
+              </div>
+    
+              <div style={{textAlign: "center"}}>
+                <Button variant="contained" color="primary" onClick={() => { ChangeCurrentOverlay("none") }} size="large">Continue</Button>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+    )
+  }
+
+  /* Overlay displaying input for stream title:
   function StreamTitleOverlay() {
     const [streamTitle, SetStreamTitle] = useState("");
 
@@ -106,9 +217,7 @@ function ProductList() {
       </div>
     );
   }
-
-  // todo
-  // finish fetch for CHECKING IF LIVE (if so, display embed iframe)
+  */
 
   // useState is needed for a new stream's information to update that HTML on <StreamCreatedOverlay/> upon their change in GoLive()
   const [newStream, SetStreamObject] = useState({key: "", url: ""});
@@ -138,9 +247,11 @@ function ProductList() {
             console.error(error);
         });*/
 
-    // FOR testing purposes:
-    storeObject.isLive = true;
-    CheckStoreObject(); // to not wait 60 seconds maybe? althought the user will prob need a minute to set up the stream on OBS studio
+    // FOR testing purposes ONLY:
+    testStreamObject.isLive = true; // this is equivalent to MAKING the Stream Database's 'isLive' field for this store be set to: TRUE
+                  // Which is done via the above 'fetch'. Then the function below is called and it checks that 'isLive' field in the Database
+
+    CheckStoreObject(); // to not wait 60 seconds maybe? although the user will prob need a minute to set up the stream on OBS studio
   }
 
   function LiveInfoBar() {
@@ -154,7 +265,9 @@ function ProductList() {
 
             <Grid item md={4} container justifyContent="flex-end" style={{color: "grey"}}>
               <Button startIcon={<CircleIcon />} variant="contained" color="error" onClick={() => {
-                ChangeTitleOverlayDisplayed("block");
+                //ChangeTitleOverlayDisplayed("block");
+
+                ChangeCurrentOverlay("setStreamTitle");
               }} size="large">Go Live</Button>
             </Grid>
           </Grid> 
@@ -168,7 +281,7 @@ function ProductList() {
               </Grid>
               <Grid item md={4} container justifyContent="flex-end" style={{color: "red"}}>
                 <CircleIcon style={{verticalAlign: 'middle', marginRight: 10}}/>
-                <p style={{alignSelf: 'center'}}><b>LIVE | </b> 3 viewers</p>
+                <p style={{alignSelf: 'center'}} onClick={() => {testStreamObject.isLive = false;}}><b>LIVE | </b> 3 viewers</p>
               </Grid>
             </Grid>
             <Grid container spacing={0} justifyContent="center" alignItems="center" direction='row' style={{marginBottom: 20}}>
@@ -195,8 +308,7 @@ function ProductList() {
         <Header/>
       </div> 
 
-      <StreamTitleOverlay />
-      <StreamCreatedOverlay />
+      <Overlay/>
 
       <LiveInfoBar />
       <Grid container direction='column'>
