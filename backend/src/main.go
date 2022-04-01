b9625b3fabfc273cea027c95b5fdbd199dc3f2a0
Yi-Ming Chang <69064626+yimingStar@users.noreply.github.com>
2022-04-01 15:14:45 -0400

    remove the auth of JWT of PA2 api

--- backend/src/main.go
+++ backend/src/main.go
@@ -52,7 +52,7 @@ func main() {
 
 	// Product
 	authApis.HandleFunc("/product/create", api.ProductCreate).Methods("POST", "OPTIONS")
-	authApis.HandleFunc("/product/{productId}/info", api.ProductRESTFUL).Methods("GET", "OPTIONS")
+	r.HandleFunc(prodRoutePrefix+"/product/{productId}/info", api.ProductRESTFUL).Methods("GET", "OPTIONS")
 	authApis.HandleFunc("/product/{productId}", api.ProductRESTFUL).Methods("POST", "PUT", "DELETE", "OPTIONS")
 	// TEST API path
 	r.HandleFunc(testRoutePrefix+"/echo", test.EchoString).Methods("GET", "OPTIONS")
