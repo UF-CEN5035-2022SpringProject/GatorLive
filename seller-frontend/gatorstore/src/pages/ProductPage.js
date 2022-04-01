f696a8aaea71e2579970d1229304cbc8f4ef2c26
Yi-Ming Chang <69064626+yimingStar@users.noreply.github.com>
2022-03-31 20:11:08 -0400

    add product get

--- seller-frontend/gatorstore/src/pages/ProductPage.js
+++ seller-frontend/gatorstore/src/pages/ProductPage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import '../styles/sellerProductPage.css';
@@ -9,7 +9,37 @@ import MoveUpIcon from '@mui/icons-material/MoveUp';
 import Button from '@mui/material/Button';
 import Avatar from '@mui/material/Avatar';
 
+import settings from '../settings'
+import { useParams, Link } from 'react-router-dom';
+
 export default function ProductPage() {
+    const [productInfo, SetProductInfo] = useState(null);
+    // Effect for overlay (to freeze scrolling when an overlay is open)
+    useEffect(() => {
+        getProductInfo()
+    }, [])
+    
+    const { productID } = useParams(); // Get StoreID string from the url
+    function getProductInfo() {
+        console.log('Get Product ID:' + productID);
+        const requestOptions = {
+            method: 'GET'
+        };
+        fetch(settings.apiHostURL + `product/${productID}/info`, requestOptions) // PA2
+        .then(response => response.json())
+        .then(response => {
+            if (response.status === 0) {
+                SetProductInfo(response.result);
+            }
+            else {
+                alert("ERROR: YouTube API did not respond with 'success' status code 0.");
+            }
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+    }
+    
     return (
         <div className="RootFlexContainer">
             <Header />
@@ -18,23 +48,34 @@ export default function ProductPage() {
                     <img src={gatorMug} alt="Gator Mug"/>
                     <Button color="primary" variant="contained"><EditIcon /></Button>
                 </div>
-                <div className="ProductKeyDetailColumn">
-                    <h1>Gator Mug</h1>
-                    <h2>$5.99</h2>
-                    <Button startIcon={<EditIcon />} color="primary" variant="contained" size="large">Edit Details</Button>
-                </div>
+                {productInfo ? 
+                    <div className="ProductKeyDetailColumn">
+                        <h1>{productInfo.name}</h1>
+                        <h2>{`${productInfo.price}`}</h2>
+                        <Button startIcon={<EditIcon />} color="primary" variant="contained" size="large">Edit Details</Button>
+                    </div> 
+                    : 
+                    null
+                }
+                
                 <div style={{flex: 1}} className="flexCenter">
                     <div className="ProductStoreInfo flexCenter colFlex">
-                        <h1>UF Store</h1>
+                        <h1>GatorStore</h1>
                         <Avatar sx={{ bgcolor: 'navy', fontSize: 50, width: 120, height: 120}}>UF</Avatar>
-                        <Button startIcon={<MoveUpIcon />} color="secondary" variant="contained" size="large" sx={{marginBottom: 3}}>Switch to Another Store</Button>
-                        <Button startIcon={<EditIcon />} color="primary" variant="contained" size="large" sx={{marginBottom: 1}}>Edit Store</Button>
+                        <Button component={Link} to={'/store-list'} startIcon={<MoveUpIcon />} color="secondary" variant="contained" size="large" sx={{marginBottom: 3}}>Back to Store List</Button>
+                        {/* <Button startIcon={<EditIcon />} color="primary" variant="contained" size="large" sx={{marginBottom: 1}}>Edit Store</Button> */}
                     </div>
                 </div>
             </div>
-            <div className="ProductDescription">
-                This is the description for this product. You should buy this.
-            </div>
+            {productInfo?
+                <div className="ProductDescription">
+                    <h2>Product Description</h2>
+                    <br></br>
+                    {productInfo.description}
+                </div>
+                :
+                null
+            }
             <Footer />
         </div>
     );
